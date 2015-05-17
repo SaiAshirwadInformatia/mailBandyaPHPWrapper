68c20e7875993223c05118e343a0bb9ea1414e66
Praveen Menezes <pmenezes@saiashirwad.com>
2015-05-17 10:54:28 +0530

    Replaced SAI_ABS_PATH with SAI_MAILBANDYA_PATH

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(
     function ($classname)
     {
-        $classpath = SAI_ABS_PATH . 'src' . DS . $classname . '.php';
+        $classpath = SAI_MAILBANDYA_PATH . 'src' . DS . $classname . '.php';
        
         if (file_exists($classpath)) {
             require_once $classpath;
